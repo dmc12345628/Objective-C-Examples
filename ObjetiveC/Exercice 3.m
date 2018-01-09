1789e7f70599bc497be7ed3f4697e10fa1d7afb4
local192 <local192@lisaac01.univ-lr.fr>
2018-01-09 09:59:36 +0100

    Exercice 2 and Exercice 3 were added

--- ObjetiveC/Exercice 3.m
+++ ObjetiveC/Exercice 3.m
@@ -1,23 +1,23 @@
 //
-//  Exercice 2.m
+//  Exercice 3.m
 //  ObjetiveC
 //
 //  Created by local192 on 09/01/2018.
 //  Copyright © 2018 local192. All rights reserved.
 //
 
-#include "Exercice 2.h"
+#include "Exercice 3.h"
 
 @interface SampleClass:NSObject
-- (void) swap:(int)num1 andNum2:(int)num2;
+- (void) swap:(int *)num1 andNum2:(int *)num2;
 @end
 
 @implementation SampleClass
-- (void)swap:(int)num1 andNum2:(int)num2 {
+- (void)swap:(int *)num1 andNum2:(int *)num2 {
     int temp;
-    temp = num1;
-    num1 = num2;
-    num2 = temp;
+    temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
 }
 @end
 
@@ -30,10 +30,12 @@ int main() {
     NSLog(@"Before swap, value of a : %d\n", a);
     NSLog(@"Before swap, value of b : %d\n", b);
     
-    [sampleClass swap:a andNum2:b];
+    [sampleClass swap:&a andNum2:&b];
     
     NSLog(@"After swap, value of a : %d\n", a);
     NSLog(@"After swap, value of b : %d\n", b);
     
     return 0;
 }
+
+
