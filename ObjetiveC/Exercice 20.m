984ca83872693ac3e3cb727605d8910240626ac2
local192 <local192@lisaac01.univ-lr.fr>
2018-01-16 13:52:38 +0100

    Exercice 21 was added

--- ObjetiveC/Exercice 20.m
+++ ObjetiveC/Exercice 20.m
@@ -13,6 +13,18 @@
 +(NSString *) getCopyRightString {
     return @"Copyright TutorialsPoint.com 2013";
 }
+-(NSString *) reverseString {
+    NSMutableString *reversedStr;
+    int len = [self length];
+    
+    reversedStr = [NSMutableString stringWithCapacity:len];
+    
+    while (len > 0)
+        [reversedStr appendString:
+         [NSString stringWithFormat:@"%C", [self characterAtIndex:--len]]];
+    
+    return reversedStr;
+}
 
 @end
 
@@ -20,6 +32,13 @@ int main() {
     @autoreleasepool {
         NSString *copyrightString = [NSString getCopyRightString];
         NSLog(@"Accessing Category : %@", copyrightString);
+        
+        NSString *str = [NSString stringWithString:@"Daniel"];
+        NSString *rev;
+        
+        NSLog(@"String: %@", str);
+        rev = [str reverseString];
+        NSLog(@"Reversed: %@", rev);
     }
     
     return 0;
